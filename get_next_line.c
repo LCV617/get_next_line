2b29f886dc8cab4afad4ae84483b48b052643988
Emilien Waltz <emilienwaltz@gmail.com>
2025-12-08 16:57:43 +0100

    on avance hein

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: ewaltz <ewaltz@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/05 14:15:18 by ewaltz            #+#    #+#             */
-/*   Updated: 2025/12/06 16:20:15 by ewaltz           ###   ########.fr       */
+/*   Updated: 2025/12/08 15:30:50 by ewaltz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int	get_index(char *str)
 	return (-1);
   while (str[i])
   {
-	if (str [i] == '\n')
+	if (str[i] == '\n')
 	  return (i);
 	i++;
   }
@@ -34,7 +34,7 @@ char	*ft_strdup(char *str, int len)
   char	*new_str;
 
   i = 0;
-  if (!str)
+  if (!str || len > ft_strlen(str) || len < 0)
 	return (NULL);
   new_str = malloc(sizeof(char) * (len + 1));
   if (!new_str)
@@ -48,40 +48,74 @@ char	*ft_strdup(char *str, int len)
   return (new_str);
 }
 
+int	ft_strlen(char *str)
+{
+  int	i;
+
+  i = 0;
+  if (!str)
+	return (0);
+  while (str[i])
+	i++;
+  return (i);
+}
 
 char	*get_rest(char *str, int start)
 {
+  int	len;
   int	i;
   char	*new_str;
 
   i = 0;
   if (!str)
 	return (NULL);
-  new_str = malloc(sizeof((char) * len) + 1);
+  len = ft_strlen(str);
+  if (start >= len)
+	return (NULL);
+  new_str = malloc(sizeof(char) * (len - start + 1));
   if (!new_str)
 	return (NULL);
   while (str[start])
-  {
-	new_str[i] = str[start];
-	start++;
-	i++;
-  }
+	new_str[i++] = str[start++];
   new_str[i] = '\0';
   return (new_str);
 }
 
 
+char	*str_join(char *rest, char *buffer)
+{
+  int	y;
+  int	i;
+  char	*new_rest;
+
+  i = 0;
+  y = 0;
+  new_rest = malloc(sizeof(char) * (ft_strlen(rest) + ft_strlen(buffer) + 1));
+  if (!new_rest)
+	return (NULL);
+  if (rest)
+  {
+	while (rest[i])
+	  new_rest[i++] = rest[i];
+  }
+  while (buffer[y])
+	new_rest[i++] = buffer[y++];
+  new_rest[i] = '\0';
+  free (rest);
+  return (new_rest);
+}
+
 
 char	*get_next_line(int fd)
 {
-  static char* rest;
-  int i;
+  static char	*rest;
+  char			buffer[BUFFER_SIZE + 1];
+  char			*ligne;
+  int			bytes;
+  int 			index;
+  int 			i;
 
-  i = get_index(rest);
-  rest = NULL;
-  if (i != -1)
-  {
-	rest = get_rest(rest, i);
-	return (ft_strdup(rest, i))
-  }
+  if (fd < 0 || BUFFER_SIZE <= 0)
+	return (NULL);
+
 }
